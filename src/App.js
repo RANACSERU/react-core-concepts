9d45a7b32cbb3a6ae1655473b97574f024009731
MD SHOHEL RANA <ranacseru37@gmail.com>
2023-06-08 08:55:28 +0600

    Dynamic value pass in React

--- src/App.js
+++ src/App.js
@@ -2,24 +2,51 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const nayoks = ['Jafor Iqbal', 'Salman Shah'];
+  const products = [
+    {name:'Photoshop', price:'$99.99'},
+    {name:'Illustrator', price:'$60'},
+    {name:'VS code', price:'$95'},
+    {name:'MySQL', price:'$999'}
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Im a REACT Person</h1>
+        <Product name={products[0].name} price={products[0].price}></Product>
+        <Product name={products[1].name} price={products[1].price}></Product>
+        <Product name={products[2].name} price={products[2].price}></Product>
+        <Product name={products[3].name} price={products[3].price}></Product>
+
+        
       </header>
     </div>
   );
+}
+
+function Product(props){
+  const productStyle = {
+    border:'1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'250px',
+    width:'200px',
+    float:'left',
+    margin:'5px'
+
+
+  }
+  return (
+    <div style={productStyle}>
+      <h2 style={{color:'blue'}}>{props.name}</h2>
+      <h4 style={{color:'red'}}>{props.price}</h4>
+      <button>Buy Now</button>
+
+    </div>
+  )
+
 }
 
 export default App;
